83efc7e1f1d7eb8113d74f057e1678e0276eb281
Steven Lawson <unluckygamblermusic@gmail.com>
2017-03-26 16:02:51 -0600

    updating stuff

--- js/creative.js
+++ js/creative.js
@@ -9,6 +9,24 @@
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
+    //js for carosel
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+    $('.fdi-Carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
