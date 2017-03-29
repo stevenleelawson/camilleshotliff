9efa752891ab0d3399af8460f7a8ed756a66402c
Steven Lawson <unluckygamblermusic@gmail.com>
2017-03-28 19:21:36 -0600

    reformatting tat photos

--- js/creative.js
+++ js/creative.js
@@ -10,24 +10,24 @@
         event.preventDefault();
     });
     //js for carosel
-    $('#myCarousel').carousel({
-        interval: 10000
-    })
-    $('.fdi-Carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-        }
-        else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
-    });
-
+    // $('#myCarousel').carousel({
+    //     interval: 10000
+    // })
+    // $('.fdi-Carousel .item').each(function () {
+    //     var next = $(this).next();
+    //     if (!next.length) {
+    //         next = $(this).siblings(':first');
+    //     }
+    //     next.children(':first-child').clone().appendTo($(this));
+    //
+    //     if (next.next().length > 0) {
+    //         next.next().children(':first-child').clone().appendTo($(this));
+    //     }
+    //     else {
+    //         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    //     }
+    // });
+    
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
